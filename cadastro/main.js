96b79bf9d5d414fe4f6d009ca75542b0d7816bbd
Andreyhs04 <andrey.hias29@gmail.com>
2023-10-05 16:52:29 -0300

    Initial commit

---
+++ cadastro/main.js
@@ -0,0 +1,6 @@
+const forms = document.querySelector('.form');
+const inputUsuario = forms.querySelector('#usuario');
+const inputEmail = forms.querySelector('#email');
+const inputTelefone = forms.querySelector('#telefone');
+const inputSenha = forms.querySelector('#senha');
+const inputCsenha = forms.querySelector('#Csenha');
---
+++ login/main.js
@@ -0,0 +1,36 @@
+//seleciona o formulario de login
+const form = document.querySelector('.login-form')
+
+const usuarioInput = form.querySelector("input[type='text']");
+
+const senhaInput = form.querySelector("input[type='password']");
+
+const EmailInput = form.querySelector("input[type='email']");
+
+const numberInput = form.querySelector("input[type='number']");
+
+form.addEventListener('submit', (event) => {
+    // Adicionar o event.preventDefault() evita que a página seja recarregada quando o botão for apertado
+    event.preventDefault();
+
+    // Obtém os valores dos campos de entrada do usuário e senha
+
+    const usuario = usuarioInput.value.trim();
+    const senha = senhaInput.value.trim();
+    const email = EmailInput.value.trim();
+    const number = numberInput.value.trim();
+    
+
+    if(usuario === '' || senha === '' ) {
+        alert("Por favor preencha os campos");
+    } else if (usuario === 'usuario' && senha === 'senha') {
+        // Verifica se as informações de login e senha estão corretas
+        alert("Bem vindo!")
+        location.replace("/tarefa/index.html", "_self")
+    } else {
+        // Exibe uma mensagem de erro se as informações estiverem, erradas
+        alert( "Usuário ou senha incorreto, tente novamente" )
+    
+    }
+
+})
